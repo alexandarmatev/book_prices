57d8133ecd99142a7f8acd4763363f6aa6d58b28
Alexanda Matev <alexandarmatev@gmail.com>
2022-08-08 13:36:03 +0300

    v1.1

--- book_prices_program.py
+++ book_prices_program.py
@@ -1,8 +1,10 @@
+# Function for getting of the book price
 def get_book_price():
     price_input = float(input("Enter a book price: $"))
     return price_input
 
 
+# Function for calculating the avg book price
 def calculate_avg_price(prices_list):
     total_books = len(prices_list)
     sum_of_book_prices = sum(prices_list)
@@ -10,33 +12,52 @@ def calculate_avg_price(prices_list):
     return avg_book_price
 
 
+# Empty list for storing of the book prices
 list_with_book_prices = []
+# While loop until the input of the user is either 0 or less
 while True:
-    book_price = get_book_price()
-    if book_price > 0:
-        list_with_book_prices.append(book_price)
-    else:
-        books_number = len(list_with_book_prices)
-        avg_price = calculate_avg_price(list_with_book_prices)
-        print(f"Book count is: {books_number}\n"
-              f"The average book price is: ${avg_price}")
-        break
+    # Catching a ZeroDivisionError error if the user's initial input is 0
+    try:
+        # Getting the book price by using the get book price function
+        book_price = get_book_price()
+        if book_price > 0:
+            # Appending the user input into the list with book prices
+            list_with_book_prices.append(book_price)
+        else:
+            # Getting the books number by using len function
+            books_number = len(list_with_book_prices)
+            # Storing the output of the avg price function into variable
+            avg_price = calculate_avg_price(list_with_book_prices)
+            # Printing the book count and the avg book price
+            print(f"Book count is: {books_number}\n"
+                  f"The average book price is: ${avg_price}")
+            # Breaking of the while loop
+            break
+    except ZeroDivisionError:
+        print("You've entered invalid amount. Try again.")
 
-
+# Creating an empty list which will store the modified book prices
 modified_book_prices = []
+# While loop until the input of the user is either 0 or less
 while True:
+    # Getting the book price by using the get book price function
     book_price = get_book_price()
     if book_price > 0:
+        # Printing the appropriate message if the price is above the avg
         if book_price > avg_price:
             print(f"${book_price} is above the avg price")
             modified_book_prices.append("Above Price")
+        # Printing the appropriate message if the price is below the avg
         elif book_price < avg_price:
             print(f"${book_price} is below the avg price")
             modified_book_prices.append("Below Price")
+        # Printing the appropriate message if the price is equal to the avg
         elif book_price == avg_price:
             print(f"${book_price} is equal to the avg price")
             modified_book_prices.append("Equal Price")
     else:
+        # Finally, printing the list with the appended values
         print(modified_book_prices)
+        # Breaking of the while loop
         break
 
