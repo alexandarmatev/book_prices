e03f2c316088a0e59fc43bcb96ec5bc29c49a039
Alexanda Matev <alexandarmatev@gmail.com>
2022-08-08 21:44:54 +0300

    v1.3

--- book_prices_program.py
+++ book_prices_program.py
@@ -23,16 +23,15 @@ def calculate_avg_price(prices_list):
 list_with_book_prices = []
 # While loop until the input of the user is either 0 or less
 while True:
-    # Catching a ZeroDivisionError error if the user's initial input is 0
     try:
         # Getting the book price by using the get book price function
         book_price = get_book_price()
         if book_price > 0:
             # Appending the user input into the list with book prices
             list_with_book_prices.append(book_price)
-        elif book_price < 0:
+        elif book_price <= 0 and not list_with_book_prices:
             print("You've entered invalid amount. Try again.")
-        else:
+        elif book_price <= 0 and list_with_book_prices:
             # Storing the outputs of the avg price function into variable
             avg_price, books_number = calculate_avg_price(list_with_book_prices)
             # Printing the book count and the avg book price
@@ -40,37 +39,38 @@ while True:
                   f"The average book price is: ${avg_price}")
             # Breaking of the while loop
             break
+    # Catching a ZeroDivisionError error if the user's initial input is 0
     except ZeroDivisionError:
+        print("You've entered invalid amount. Try again.")
+    # Catching a ValueError error if the user's initial input is different from integer
+    except ValueError:
         print("You've entered invalid amount. Try again.")
 
 # Creating an empty dictionary which will map the book prices
 mapped_book_prices = {}
 # While loop until the input of the user is either 0 or less
 while True:
-    # Getting the book price by using the get book price function
-    book_price = get_book_price()
-    if book_price > 0:
-        # Printing the appropriate message if the price is above the avg
-        if book_price > avg_price:
-            print(f"${book_price} is above the avg price")
-            mapped_book_prices[book_price] = "above price"
-        # Printing the appropriate message if the price is below the avg
-        elif book_price < avg_price:
-            print(f"${book_price} is below the avg price")
-            mapped_book_prices[book_price] = "below price"
-        # Printing the appropriate message if the price is equal to the avg
-        elif book_price == avg_price:
-            print(f"${book_price} is equal to the avg price")
-            mapped_book_prices[book_price] = "equal price"
-    elif book_price < 0:
-        print("You've entered invalid amount. Try again.")
-    else:
-        # If the list is empty
-        if not mapped_book_prices:
-            print("There are no prices to show. Try with entering a book price.")
-        # Finally, printing the dictionary with the appended values
-        else:
+    try:
+        # Getting the book price by using the get book price function
+        book_price = get_book_price()
+        if book_price > 0:
+            # Printing the appropriate message if the price is above the avg
+            if book_price > avg_price:
+                print(f"${book_price} is above the avg price")
+                mapped_book_prices[f"${book_price}"] = "above avg price"
+            # Printing the appropriate message if the price is below the avg
+            elif book_price < avg_price:
+                print(f"${book_price} is below the avg price")
+                mapped_book_prices[f"${book_price}"] = "below avg price"
+            # Printing the appropriate message if the price is equal to the avg
+            elif book_price == avg_price:
+                print(f"${book_price} is equal to the avg price")
+                mapped_book_prices[f"${book_price}"] = "equal to avg price"
+        elif book_price <= 0 and not mapped_book_prices:
+            print("There are no prices to compare. Try with entering a valid book price.")
+        elif book_price <= 0 and mapped_book_prices:
             print(f"The final dictionary with the prices is: {mapped_book_prices}")
-            # Breaking of the while loop
             break
+    except ValueError:
+        print("You've entered invalid amount. Try again.")
 
